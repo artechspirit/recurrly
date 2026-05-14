2c987b3e031bc6230217985f2f1d6e7ad9021df0
artechspirit <artechspirit@gmail.com>
2026-05-14 22:37:52 +0700

    feat: implements fonts

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -7,27 +7,25 @@ const SafeAreaView = styled(RNSafeAreaView);
 export default function Index() {
   return (
     <SafeAreaView className="flex-1 bg-background p-5">
-      <Text className="text-xl font-bold text-success">
-        Welcome to Nativewind!
-      </Text>
+      <Text className="text-primary font-sans-extrabold text-7xl">Home</Text>
 
       <Link
         href={"/onboarding"}
-        className="mt-4 rounded bg-primary text-white p-4"
+        className="mt-4 rounded font-sans-bold bg-primary text-white p-4"
       >
         Go to onboarding
       </Link>
 
       <Link
         href={"/(auth)/sign-in"}
-        className="mt-4 rounded bg-primary text-white p-4"
+        className="mt-4 rounded font-sans-bold bg-primary text-white p-4"
       >
         Go to Sign In
       </Link>
 
       <Link
         href={"/(auth)/sign-up"}
-        className="mt-4 rounded bg-primary text-white p-4"
+        className="mt-4 rounded font-sans-bold bg-primary text-white p-4"
       >
         Go to Sign Up
       </Link>
